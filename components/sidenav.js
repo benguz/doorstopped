adff3ef9c4d860f5c87434ab94192d9821053049
benguz <84636906+benguz@users.noreply.github.com>
2023-04-26 15:46:28 -0400

    gdocs test

--- components/sidenav.js
+++ components/sidenav.js
@@ -1,4 +1,8 @@
-const items = [
+const sidenav = document.querySelector('#sidenav');
+const toc = document.querySelector('#toc');
+
+  // Define the items for the side navigation and the headings for the table of contents
+  const items = [
     {
       title: 'Leveraged',
       links: [
@@ -10,90 +14,46 @@ const items = [
     {
       title: 'Doorstops: How to...',
       links: [
-        { href: '/doorstops/extension', children: 'Ask for an extension' },
-        { href: '/doorstops/getout', children: 'Take a break' },
-        { href: '/doorstops/mentor', children: 'Find a mentor' },
-        { href: '/doorstops/listen', children: 'Listen and change' }
+        { href: '/doorstops/extension.html', children: 'Ask for an extension' },
+        { href: '/doorstops/getout.html', children: 'Take a break' },
+        { href: '/doorstops/mentor.html', children: 'Find a mentor' },
+        { href: '/doorstops/listen.html', children: 'Listen and change' }
       ]
     },
   ];
-  
-  function SideNav() {
-    const router = { pathname: window.location.pathname };
-  
-    const nav = document.createElement('nav');
-    nav.classList.add('sidenav');
-    nav.style.position = 'sticky';
-    nav.style.top = 'var(--nav-height)';
-    nav.style.height = 'calc(100vh - var(--nav-height))';
-    nav.style.flex = '0 0 240px';
-    nav.style.overflowY = 'auto';
-    nav.style.padding = '2rem 0 2rem 2rem';
-  
-    items.forEach((item) => {
-      const div = document.createElement('div');
-      const h3 = document.createElement('h3');
-      h3.innerText = item.title;
-      div.appendChild(h3);
-  
-      const ul = document.createElement('ul');
-      ul.classList.add('flex', 'column');
-      item.links.forEach((link) => {
-        const li = document.createElement('li');
-        li.style.listStyleType = 'none';
-        li.style.margin = '0 0 0.7rem 0.7rem';
-        li.style.fontSize = '14px';
-        li.style.fontWeight = '400';
-  
-        const a = document.createElement('a');
-        a.href = link.href;
-        a.innerText = link.children;
-        a.style.textDecoration = 'none';
-  
-        if (router.pathname === link.href) {
-          li.classList.add('active');
-          a.style.textDecoration = 'underline';
-        }
-  
-        li.appendChild(a);
-        ul.appendChild(li);
-      });
-  
-      div.appendChild(ul);
-      nav.appendChild(div);
-    });
-  
-    const style = document.createElement('style');
-    style.innerHTML = `
-      nav {
-        /* https://stackoverflow.com/questions/66898327/how-to-keep-footer-from-pushing-up-sticky-sidebar */
-        position: sticky;
-        top: var(--nav-height);
-        height: calc(100vh - var(--nav-height));
-        flex: 0 0 240px;
-        overflow-y: auto;
-        padding: 2rem 0 2rem 2rem;
-      }
-      h3 {
-        font-weight: 500;
-        margin: 0.5rem 0 0;
-        padding-bottom: 0.5rem;
-      }
-      ul {
-        margin: 0;
-        padding: 0;
-      }
-      li a:hover,
-      li.active > a {
-        text-decoration: underline;
-      }
-      @media screen and (max-width: 600px) {
-        nav {
-          display: none;
-        }
-      }
+
+  // Generate the HTML for the side navigation and append it to the sidenav element
+  const sidenavHtml = `
+    <nav class="sidenav">
+      ${items.map(item => `
+        <div>
+          <h3>${item.title}</h3>
+          <ul class="flex column">
+            ${item.links.map(link => `
+              <li ${window.location.pathname === link.href ? 'class="active"' : ''}>
+                <a href="${link.href}">${link.children}</a>
+              </li>
+            `).join('')}
+          </ul>
+        </div>
+      `).join('')}
+    </nav>
+  `;
+  sidenav.innerHTML = sidenavHtml;
+
+  // Generate the HTML for the table of contents and append it to the toc element
+  const headings = Array.from(document.querySelectorAll('h2, h3'));
+  if (headings.length > 0) {
+    const tocHtml = `
+      <nav class="toc">
+        <ul class="flex column">
+          ${headings.map(heading => `
+            <li class="${heading.tagName.toLowerCase()}">
+              <a href="#${heading.id}">${heading.textContent}</a>
+            </li>
+          `).join('')}
+        </ul>
+      </nav>
     `;
-  
-    nav.appendChild(style);
-    return nav;
+    toc.innerHTML = tocHtml;
   }
