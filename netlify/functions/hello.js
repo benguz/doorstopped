6bc1d2ac178c1b6cda10ad62ed19de99ff1995a0
benguz <84636906+benguz@users.noreply.github.com>
2023-07-05 15:56:41 -0400

    analytics

---
+++ netlify/functions/hello.js
@@ -0,0 +1,7 @@
+// netlify/functions/hello.js
+exports.handler = async function(event, context) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: "Hello, World"})
+    };
+  }
