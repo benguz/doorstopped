997187d93ea12129f1539e15825dbb845872a20b
benguz <84636906+benguz@users.noreply.github.com>
2023-07-05 22:22:53 -0400

    global test

--- netlify/functions/dashboard.js
+++ netlify/functions/dashboard.js
@@ -1,42 +1,29 @@
 const axios = require("axios");
 
+console.log("axios checked");
+
 exports.handler = async function(event, context) {
-  const API_KEY = process.env.main; // Your API key is stored as an environment variable
-
-  const response = await axios.get('https://plausible.io/api/v1/stats/realtime/visitors?site_id=doorstopped.org', {
-    headers: {
-      'Authorization': `Bearer ${API_KEY}`
+    const API_KEY = process.env.TESTING_KEY;    
+    console.log('API_KEY:', API_KEY); // checking correctness
+    try {
+      const response = await axios.get('https://plausible.io/api/v1/stats/realtime/visitors?site_id=doorstopped.org', {
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`
+        }
+      });
+  
+      return {
+        statusCode: 200,
+        body: JSON.stringify(response.data)
+      };
+    } catch (error) {
+      console.error('Axios request failed:', error);
+  
+      // Return the error message in the response
+      return {
+        statusCode: 500,
+        body: `Error: ${error.message || error}`
+      };
     }
-  });
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response.data)
-  };
-}
-
-// exports.handler = async function(event, context) {
-//     const API_KEY = process.env.main; // Your API key is stored as an environment variable
-  
-//     try {
-//       const response = await axios.get('https://plausible.io/api/v1/stats/realtime/visitors?site_id=doorstopped.org', {
-//         headers: {
-//           'Authorization': `Bearer ${API_KEY}`
-//         }
-//       });
-  
-//       return {
-//         statusCode: 200,
-//         body: JSON.stringify(response.data)
-//       };
-//     } catch (error) {
-//       console.error('Axios request failed:', error);
-  
-//       // Return the error message in the response
-//       return {
-//         statusCode: 500,
-//         body: `Error: ${error.message || error}`
-//       };
-//     }
-//   }
+  }
   
