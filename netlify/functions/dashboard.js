a3045a12f10f9a59077860f2478902bcc8ea76ed
benguz <84636906+benguz@users.noreply.github.com>
2023-07-06 15:16:47 -0400

    survey data test

--- netlify/functions/dashboard.js
+++ netlify/functions/dashboard.js
@@ -1,15 +1,24 @@
 const axios = require("axios");
 
-console.log("axios checked");
-
 exports.handler = async function(event, context) {
     const API_KEY = process.env.TESTING_KEY;    
-    console.log('API_KEY:', API_KEY); // checking correctness
+    // event:props:culture
+    // https://plausible.io/api/v1/stats/breakdown?site_id=doorstopped.org&period=6mo&property=event:props:method&filters=event:name%3D%3DDownload'
+    const params = {
+      site_id: 'doorstopped.org',
+      period: '6mo',
+      property: 'event:name',
+      metrics: 'events',
+      filters: 'event:name==culture'
+    };
+    // https://plausible.io/api/v1/stats/breakdown
+    // 
     try {
-      const response = await axios.get('https://plausible.io/api/v1/stats/realtime/visitors?site_id=doorstopped.org', {
+      const response = await axios.get('https://plausible.io/api/v1/stats/breakdown?site_id=doorstopped.org&period=12mo&property=event:name', {
         headers: {
           'Authorization': `Bearer ${API_KEY}`
-        }
+        },
+        // params: params
       });
   
       return {
