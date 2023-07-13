315265733483a87eb4a8b3e1c8365f4e4dd08e7e
benguz <84636906+benguz@users.noreply.github.com>
2023-07-13 11:10:06 -0400

    ref v1, new skills, getout improvements

--- assets/js/skill.js
+++ assets/js/skill.js
@@ -1,12 +1,24 @@
 document.querySelectorAll('.aesthetic-button').forEach((button, index) => {
     button.addEventListener('click', function(){
       if (button.textContent === "Negotiating") {
-        window.location.href = "/skills/negotiating.html";
+        window.location.href = "/skills/negotiating";
 
       } else if (button.textContent === "Networking") {
-        window.location.href = "/skills/networking.html";
+        window.location.href = "/skills/networking";
       } else if (button.textContent === "Building a brand") {
-        window.location.href = "/skills/brand.html";
+        window.location.href = "/skills/brand";
+      } else if (button.textContent === "Emotional Availability") {
+        window.location.href = "/skills/emotions";
+      } else if (button.textContent === "Proactive Communication") {
+          window.location.href = "/skills/comms";
+      } else if (button.textContent === "Pitching yourself") {
+        window.location.href = "/skills/pitch";
+      } else if (button.textContent === "Comfort under stress") {
+        window.location.href = "/skills/stress";
+      } else if (button.textContent === "Building relationships") {
+        window.location.href = "/skills/relationships";
+      } else if (button.textContent === "Subversion") {
+        window.location.href = "/skills/subversion";
       }
     });
   });
