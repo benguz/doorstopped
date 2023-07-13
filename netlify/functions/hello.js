fb8f02e0c51b108517ea7b3ea6ca6f37809b812c
benguz <84636906+benguz@users.noreply.github.com>
2023-07-13 15:01:09 -0400

    emotion word beta

--- netlify/functions/hello.js
+++
@@ -1,7 +0,0 @@
-// netlify/functions/hello.js
-exports.handler = async function(event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({message: "Hello, World"})
-    };
-  }
