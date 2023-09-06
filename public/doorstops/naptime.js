5a17f442865b8a9b858d01775b416c0ea76bfc0e
benguz <84636906+benguz@users.noreply.github.com>
2023-09-06 16:48:20 -0400

    mega commit

---
+++ public/doorstops/naptime.js
@@ -0,0 +1,31 @@
+
+$(document).ready(function() {
+    console.log("ready!");
+    var animations = ['floatPath2', 'floatPath3', 'floatPath4']; // Add more animations as needed
+    $('*').each(function() {
+        if ($(this).height() < $(this)[0].scrollHeight) {
+            $(this).scroll(function() {    
+                console.log("scrolling!");
+
+            if (Math.random() < 0.22) {
+
+            /* Apply the animation */
+            Array.from(document.querySelectorAll('.emoji-element')).forEach((element) =>
+            {
+                var animation = animations[Math.floor(Math.random() * animations.length)];
+                let newElement = $(`<span class='floating-emoji'>💤</span>`);
+
+                /* Apply the animation */
+                newElement.css('animation', `${animation} 1s forwards`);
+                
+                $(element).parent().append(newElement);
+                setTimeout(function() {
+                newElement.remove();
+                }, 2000);
+            });
+        }
+
+            });
+};
+});
+});
